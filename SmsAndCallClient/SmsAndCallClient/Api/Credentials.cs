a8e7028086d6946a6f08990d6060c48b751ee27c
Matthias Beerens <beerens.matthias@hotmail.com>
2017-07-04 15:26:56 +0200

    Removed unused usings

--- SmsAndCallClient/SmsAndCallClient/Api/Credentials.cs
+++ SmsAndCallClient/SmsAndCallClient/Api/Credentials.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SmsAndCallClient.Api
+﻿namespace SmsAndCallClient.Api
 {
     public static class Credentials
     {
