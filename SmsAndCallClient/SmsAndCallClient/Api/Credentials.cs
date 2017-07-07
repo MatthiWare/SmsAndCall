87eebad8bd5bb0b93515de9be88af6e210f41de8
Matthias Beerens <beerens.matthias@hotmail.com>
2017-07-07 15:08:36 +0200

    remove credentials

--- SmsAndCallClient/SmsAndCallClient/Api/Credentials.cs
+++ SmsAndCallClient/SmsAndCallClient/Api/Credentials.cs
@@ -2,8 +2,8 @@
 {
     internal static class Credentials
     {
-        internal const string TWILIO_ACC_SID = "***REMOVED***";
-        internal const string TWILIO_AUTH_TOKEN = "***REMOVED***";
-        internal const string CLICKATELL_API_KEY = "";
+        internal const string TWILIO_ACC_SID = "your sid token";
+        internal const string TWILIO_AUTH_TOKEN = "secret";
+        internal const string CLICKATELL_API_KEY = "secret";
     }
 }
